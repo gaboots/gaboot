6992e9218ab33f53744b37bfe600443109c8ac61
rn <associate.ru@outlook.com>
2017-05-22 18:04:06 +0700

    Modified resources/views/index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel Angular Material Starter</title>
+    <title>Onsigbaar</title>
 
     <meta name="theme-color" content="#0690B7">
 
@@ -20,9 +20,7 @@
 <body>
 
     <app-shell>
-        <div id="app-shell-header">
-            <img src="img/icons/logo.svg" width="171" height="41">
-        </div>
+        <div id="app-shell-header"></div>
         <div id="app-shell-content"></div>
     </app-shell>
 
